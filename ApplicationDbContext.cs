f34b02ee01bc325bc6ff9e2cef427f81be6e6b91
kaurr436 <155866608+kaurr436@users.noreply.github.com>
2024-06-09 21:58:38 -0600

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityAssignment
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<BlogType> BlogTypes { get; set; }
+        public DbSet<PostType> PostTypes { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<User>().HasData(
+                new User { UserId = 1, UserName = "raman", Email = "raman34451@gmail.com", PhoneNumber = "34567788" },
+                new User { UserId = 2, UserName = "aman", Email = "amam345@gmail.com", PhoneNumber = "456788888" },
+                new User { UserId = 3, UserName = "mandeep", Email = "mandeep34565@gmail.com", PhoneNumber = "789655555" }
+            );
+
+            modelBuilder.Entity<Post>().HasData(
+                new Post { PostId = 1, Title = "post", Content = "my first assignment", UserId = 1 }
+            );
+        }
+    }
+
+}
+        
+    
---
+++ BlogType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityAssignment
+{
+    public class BlogType
+    {
+        public int BlogTypeId { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
